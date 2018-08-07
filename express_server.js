f1693f187569a933b9ba5b2bdeec2e8368d67400
Guy <guy.mcauliffe@icloud.com>
2018-08-07 20:05:22 +0000

    Sending data to urls_index.ejs by route handler

--- express_server.js
+++ express_server.js
@@ -2,23 +2,37 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs'); // set the view engine to ejs
+
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
