513337944a01b0c097c29a2b3938c9524c9bf5de
Guy <guy.mcauliffe@icloud.com>
2018-08-08 02:45:42 +0000

    POST route, generateRandomString created

--- express_server.js
+++ express_server.js
@@ -13,11 +13,19 @@ const urlDatabase = {
 //  PostRequest: req.body.longURL
 };
 
-// use res.render to load up an ejs view file
+function generateRandomString() {
+  let text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 6; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  };
+  return text;
+}
+
 
 // index page
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    res.render('pages/index');    // use res.render to load up an ejs view file
 });
 
 // about page
@@ -29,21 +37,32 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// URLS index page
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
   res.render("pages/urls_index", templateVars);
 });
 
+// new URL page
 app.get("/urls/new", (req, res) => {
   res.render("pages/urls_new");
 });
 
+// single URL Id page
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
-    shortURL: req.params.id,
+    URLId: req.params.id,
     longURL: urlDatabase[req.params.id]
   };
   res.render("pages/urls_show", templateVars);
+});
+
+// POST route
+app.post("/urls", (req, res) => {
+  let URLId = generateRandomString();
+  let URL = req.body.longURL;
+  urlDatabase[URLId][URL];
+  res.redirect(`http://localhost:8080/urls/${URLId}`);         // Respond with 'Ok' (we will replace this)
 });
 
 
