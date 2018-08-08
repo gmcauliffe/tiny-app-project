54adb3ed0346947d570757b79ea054eddda57517
Guy <guy.mcauliffe@icloud.com>
2018-08-08 02:02:26 +0000

    Added urls_new and GET route in server

--- express_server.js
+++ express_server.js
@@ -1,13 +1,16 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs'); // set the view engine to ejs
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+//  PostRequest: req.body.longURL
 };
 
 // use res.render to load up an ejs view file
@@ -29,6 +32,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
   res.render("pages/urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("pages/urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
